99bd0f1809feb10a1a9beb4ff0084d8bbd58d841
linuxspring <381263761@qq.com>
2018-10-26 16:44:22 +0800

    init ocmmit

--- conf/settings_development.py
+++ conf/settings_development.py
@@ -19,10 +19,10 @@ from settings import APP_ID
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',  # 默认用mysql
-        'NAME': 'itsm',                        # 数据库名 (默认与APP_ID相同)
-        'USER': 'root',                        # 你的数据库user
-        'PASSWORD': 'oa.oracle',                        # 你的数据库password
-        'HOST': '127.0.0.1',                   # 开发的时候，使用localhost
+        'NAME': 'gzcss',                        # 数据库名 (默认与APP_ID相同)
+        'USER': 'root',                            # 你的数据库user
+        'PASSWORD': '@86Dj3Y(it',                        # 你的数据库password
+        'HOST': '172.16.3.214',                   		   # 数据库HOST
         'PORT': '3306',                        # 默认3306
     },
 }
--- conf/settings_testing.py
+++ conf/settings_testing.py
@@ -19,10 +19,10 @@ from settings import APP_ID
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',  # 默认用mysql
-        'NAME': 'itsm',                        # 数据库名 (默认与APP_ID相同)
+        'NAME': 'gzcss',                        # 数据库名 (默认与APP_ID相同)
         'USER': 'root',                            # 你的数据库user
-        'PASSWORD': 'oa.oracle',                        # 你的数据库password
-        'HOST': '172.16.3.184',                   		   # 数据库HOST
+        'PASSWORD': '@86Dj3Y(it',                        # 你的数据库password
+        'HOST': '172.16.3.214',                   		   # 数据库HOST
         'PORT': '3306',                        # 默认3306
     },
 }
